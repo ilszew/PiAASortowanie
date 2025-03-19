7c60db3a85cd25c281758ed3efc72c80d39006f6
Kornel Lewandowski <280101@student.pwr.edu.pl>
2025-03-19 13:42:16 +0100

    generation method added

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <vector>
+#include <random>
+#include <fstream>
+#include <algorithm>
 #include "mergesort.h"
 #include "quicksort.h"
 #include "introsort.h"
 
+std::vector<int> generateRandomArray(size_t size) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dist(0, size);
 
+    std::vector<int> tablica(size);
+    for (size_t i = 0; i < size; i++) {
+        tablica[i] = dist(gen);
+    }
+    return tablica;
+}
 
 int main() {
 
