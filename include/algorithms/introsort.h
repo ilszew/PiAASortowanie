951216984d4040f560d2b032ff9e460113eb6e52
Kornel Lewandowski <280101@student.pwr.edu.pl>
2025-03-19 10:04:19 +0100

    heapsort and introsort have been added

--- include/algorithms/introsort.h
+++ include/algorithms/introsort.h
@@ -1,6 +1,10 @@
 #ifndef SORTING_ALGORITHMS_INTROSORT_H
 #define SORTING_ALGORITHMS_INTROSORT_H
 #include <vector>
+#include <cmath>
+#include "quicksort.h"
+#include "insertsort.h"
+#include "heapsort.h"
 
 // sortowanie introspektywne
 
@@ -10,6 +14,26 @@ class IntroSort
 public:
     void sort(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end)
     {
-    };
+        int maxDepth = 2 * std::log(std::distance(start, end));
+        introsort(start, end, maxDepth);
+    }
+private:
+    void introsort(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end, int depth)
+    {
+        if (std::distance(start, end) <= 16)
+        {
+            InsertSort<T> insertSort;
+            insertSort.sort(start, end);
+            return;
+        }
+        if (depth == 0)
+        {
+            HeapSort<T> heapSort;
+            heapSort.sort(start, end);
+            return;
+        }
+        QuickSort<T> quickSort;
+        quickSort.sort(start, end);
+    }
 };
 #endif //SORTING_ALGORITHMS_INTROSORT_H
